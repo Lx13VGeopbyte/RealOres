921b0be5c8f3a7845682ca024f039761060ec045
L4GxT72 <nguy8181@nkcschools.org>
2013-09-20 21:52:54 -0500

    Updated to work with the newer versions with the newest oregen.

--- RealOres/init.lua
+++ RealOres/init.lua
@@ -405,52 +405,209 @@ minetest.register_craft({
 	}
 })
 
-local function generate_ore(name, wherein, minp, maxp, seed, chunks_per_volume, chunk_size, ore_per_chunk, height_min, height_max)
-	if maxp.y < height_min or minp.y > height_max then
-		return
-	end
-	local y_min = math.max(minp.y, height_min)
-	local y_max = math.min(maxp.y, height_max)
-	local volume = (maxp.x-minp.x+1)*(y_max-y_min+1)*(maxp.z-minp.z+1)
-	local pr = PseudoRandom(seed)
-	local num_chunks = math.floor(chunks_per_volume * volume)
-	local inverse_chance = math.floor(chunk_size*chunk_size*chunk_size / ore_per_chunk)
-	--print("generate_ore num_chunks: "..dump(num_chunks))
-	for i=1,num_chunks do
-		local y0 = pr:next(y_min, y_max-chunk_size+1)
-		if y0 >= height_min and y0 <= height_max then
-			local x0 = pr:next(minp.x, maxp.x-chunk_size+1)
-			local z0 = pr:next(minp.z, maxp.z-chunk_size+1)
-			local p0 = {x=x0, y=y0, z=z0}
-			for x1=0,chunk_size-1 do
-			for y1=0,chunk_size-1 do
-			for z1=0,chunk_size-1 do
-				if pr:next(1,inverse_chance) == 1 then
-					local x2 = x0+x1
-					local y2 = y0+y1
-					local z2 = z0+z1
-					local p2 = {x=x2, y=y2, z=z2}
-					if minetest.env:get_node(p2).name == wherein then
-						minetest.env:set_node(p2, {name=name})
-					end
-				end
-			end
-			end
-			end
-		end
-	end
-	--print("generate_ore done")
-end
+minetest.register_ore({
+	ore_type       = "scatter",
+	ore            = "RealOres:columbite_ore",
+	wherein        = "default:stone",
+	clust_scarcity = 15*15*15,
+	clust_num_ores = 4,
+	clust_size     = 2,
+	height_min     = -31000,
+	height_max     = -128,
+})
 
-minetest.register_on_generated(
-function(minp, maxp, seed)
-	generate_ore('RealOres:columbite_ore',       "default:stone", minp, maxp, seed+1,  1/18/18/18/18/18, 4,50, -3000,   -30)
-	generate_ore('RealOres:cerussite_ore',       "default:stone", minp, maxp, seed+2,  1/8/8/8/8/8, 4,50, -6000,   12)
-	generate_ore('RealOres:zincite_ore',         "default:stone", minp, maxp, seed+3,  1/8/8/8/8/8, 4,50, -31000,  -12)
-	generate_ore('RealOres:rhodochronite_ore',   "default:stone", minp, maxp, seed+4,  1/8/8/8/8/8, 4,50, -3000,   -6)
-	generate_ore('RealOres:vanadinite_ore',      "default:stone", minp, maxp, seed+5,  1/8/8/8/8/8, 4,50, -31000,  24)
-	generate_ore('RealOres:amazonite_ore',       "default:stone", minp, maxp, seed+6,  1/8/8/8/8/8, 4,50, -6000,   -50)
-	generate_ore('RealOres:neptunite_ore',       "default:stone", minp, maxp, seed+7,  1/16/16/16/16/16, 4,50, -31000,  -20)
-	generate_ore('RealOres:orpiment_ore',        "default:stone", minp, maxp, seed+8,  1/18/18/18/18/18, 4,50, -3000,   -36)
-	generate_ore('RealOres:tsavorite_ore',       "default:stone", minp, maxp, seed+9,  1/24/24/24/24/24, 4,50, -31000,  -100)
-end)
+minetest.register_ore({
+	ore_type       = "scatter",
+	ore            = "RealOres:columbite_ore",
+	wherein        = "default:stone",
+	clust_scarcity = 18*18*18,
+	clust_num_ores = 3,
+	clust_size     = 3,
+	height_min     = -31000,
+	height_max     = -100,
+	flags          = "absheight",
+})
+
+minetest.register_ore({
+	ore_type       = "scatter",
+	ore            = "RealOres:cerussite_ore",
+	wherein        = "default:stone",
+	clust_scarcity = 13*13*13,
+	clust_num_ores = 4,
+	clust_size     = 2,
+	height_min     = -6000,
+	height_max     = -12,
+})
+
+minetest.register_ore({
+	ore_type       = "scatter",
+	ore            = "RealOres:cerussite_ore",
+	wherein        = "default:stone",
+	clust_scarcity = 14*14*14,
+	clust_num_ores = 5,
+	clust_size     = 3,
+	height_min     = -6000,
+	height_max     = 0,
+	flags          = "absheight",
+})
+
+minetest.register_ore({
+	ore_type       = "scatter",
+	ore            = "RealOres:zincite_ore",
+	wherein        = "default:stone",
+	clust_scarcity = 8*8*8,
+	clust_num_ores = 4,
+	clust_size     = 2,
+	height_min     = -6000,
+	height_max     = -32,
+})
+
+minetest.register_ore({
+	ore_type       = "scatter",
+	ore            = "RealOres:zincite_ore",
+	wherein        = "default:stone",
+	clust_scarcity = 10*10*10,
+	clust_num_ores = 5,
+	clust_size     = 3,
+	height_min     = -8000,
+	height_max     = -12,
+	flags          = "absheight",
+})
+
+minetest.register_ore({
+	ore_type       = "scatter",
+	ore            = "RealOres:rhodochrosite_ore",
+	wherein        = "default:stone",
+	clust_scarcity = 14*14*14,
+	clust_num_ores = 4,
+	clust_size     = 2,
+	height_min     = -8750,
+	height_max     = -64,
+})
+
+minetest.register_ore({
+	ore_type       = "scatter",
+	ore            = "RealOres:rhodochrosite_ore",
+	wherein        = "default:stone",
+	clust_scarcity = 15*15*15,
+	clust_num_ores = 5,
+	clust_size     = 3,
+	height_min     = -7520,
+	height_max     = -60,
+	flags          = "absheight",
+})
+
+minetest.register_ore({
+	ore_type       = "scatter",
+	ore            = "RealOres:vanadinite_ore",
+	wherein        = "default:stone",
+	clust_scarcity = 15*15*15,
+	clust_num_ores = 3,
+	clust_size     = 1,
+	height_min     = -6000,
+	height_max     = -96,
+})
+
+minetest.register_ore({
+	ore_type       = "scatter",
+	ore            = "RealOres:vanadinite_ore",
+	wherein        = "default:stone",
+	clust_scarcity = 16*16*16,
+	clust_num_ores = 5,
+	clust_size     = 2,
+	height_min     = -6000,
+	height_max     = -80,
+	flags          = "absheight",
+})
+
+minetest.register_ore({
+	ore_type       = "scatter",
+	ore            = "RealOres:amazonite_ore",
+	wherein        = "default:stone",
+	clust_scarcity = 13*13*13,
+	clust_num_ores = 4,
+	clust_size     = 2,
+	height_min     = -1250,
+	height_max     = -90,
+})
+
+minetest.register_ore({
+	ore_type       = "scatter",
+	ore            = "RealOres:amazonite_ore",
+	wherein        = "default:stone",
+	clust_scarcity = 14*14*14,
+	clust_num_ores = 5,
+	clust_size     = 3,
+	height_min     = -1000,
+	height_max     = -70,
+	flags          = "absheight",
+})
+
+minetest.register_ore({
+	ore_type       = "scatter",
+	ore            = "RealOres:neptunite_ore",
+	wherein        = "default:stone",
+	clust_scarcity = 20*20*20,
+	clust_num_ores = 4,
+	clust_size     = 2,
+	height_min     = -30000,
+	height_max     = -256,
+})
+
+minetest.register_ore({
+	ore_type       = "scatter",
+	ore            = "RealOres:neptunite_ore",
+	wherein        = "default:stone",
+	clust_scarcity = 25*25*25,
+	clust_num_ores = 5,
+	clust_size     = 3,
+	height_min     = -31000,
+	height_max     = -128,
+	flags          = "absheight",
+})
+
+minetest.register_ore({
+	ore_type       = "scatter",
+	ore            = "RealOres:orpiment_ore",
+	wherein        = "default:stone",
+	clust_scarcity = 8*8*8,
+	clust_num_ores = 4,
+	clust_size     = 2,
+	height_min     = -100,
+	height_max     = 28,
+})
+
+minetest.register_ore({
+	ore_type       = "scatter",
+	ore            = "RealOres:orpiment_ore",
+	wherein        = "default:stone",
+	clust_scarcity = 10*10*10,
+	clust_num_ores = 5,
+	clust_size     = 3,
+	height_min     = -100,
+	height_max     = 0,
+	flags          = "absheight",
+})
+
+minetest.register_ore({
+	ore_type       = "scatter",
+	ore            = "RealOres:tsavorite_ore",
+	wherein        = "default:stone",
+	clust_scarcity = 13*13*13,
+	clust_num_ores = 4,
+	clust_size     = 2,
+	height_min     = -6000,
+	height_max     = -12,
+})
+
+minetest.register_ore({
+	ore_type       = "scatter",
+	ore            = "RealOres:tsavorite_ore",
+	wherein        = "default:stone",
+	clust_scarcity = 14*14*14,
+	clust_num_ores = 5,
+	clust_size     = 3,
+	height_min     = -6000,
+	height_max     = -12,
+	flags          = "absheight",
+})
